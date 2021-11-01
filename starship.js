94065c9545e4b3a4692aaf5c57818ccd42d43cd1
C288 <46853426+C288@users.noreply.github.com>
2021-11-01 11:54:52 -0500

    Update starship.js

--- starship.js
+++ starship.js
@@ -14,33 +14,33 @@ function drawSquare(){
     context.fillStyle = "#FF0000";
     context.fillRect(x, y, 20, 20);
 }
-    
+
+
+function drawframe(){
+    drawBackground();
+    drawSquare();
+}
+setInterval(drawframe, 30);
+
 function onkeydown(e){
     if (e.key === "ArrowLeft") {
         if (x > 0){
             x -= 5;
         }
     }
-    //top
     else if (e.key === "ArrowUp") {
         if (y > 0){
             y -= 5;
         }
     }
-    //right
     else if (e.key === "ArrowRight") {
         if (x < 380){
             x += 5;
         }
     }
-    //bottom
     else if (e.key === "ArrowDown") {
         if (y < 380){
             y += 5;
         }
     }
-    drawBackground();
-    drawSquare();
 }
-drawBackground();
-drawSquare();
